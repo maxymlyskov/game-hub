11a30ea6f07edfb471a8198801dd7de2e4f583f2
maxymlyskov <raypember02@gmail.com>
2023-05-02 16:09:56 +0300

    Installed Chakra UI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import { ListGroup } from "./components/ListGroup";
-import Form from "./components/Form";
+import { Button } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <ListGroup />
-      <Form/>
-    </div>
-  );
+  return <Button>Send</Button>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
