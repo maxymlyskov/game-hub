bd1355c3cb67295953cf614a9e253b5fe449e262
maxymlyskov <raypember02@gmail.com>
2023-05-03 16:41:59 +0300

    Add platforms selector.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlatformsSelector from "./components/PlatformsSelector";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
@@ -30,6 +31,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
+        <PlatformsSelector />
         <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
