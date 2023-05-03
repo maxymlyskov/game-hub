c4c312479c663e8b2d090d6f41ad45b1020be8d1
maxymlyskov <raypember02@gmail.com>
2023-05-03 16:57:16 +0300

    Add filter by platforms.

--- src/components/PlatformsSelector.tsx
+++ src/components/PlatformsSelector.tsx
@@ -1,8 +1,16 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import usePlatforms from "../hooks/usePlatforms";
+import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
-const PlatformsSelector = () => {
+interface Props {
+  selectedPlatform: Platform | null;
+  onSelectedPlatforms: (platform: Platform | null) => void;
+}
+
+const PlatformsSelector = ({
+  selectedPlatform,
+  onSelectedPlatforms,
+}: Props) => {
   const { data, error } = usePlatforms();
 
   if (error) return null;
@@ -10,11 +18,16 @@ const PlatformsSelector = () => {
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Platforms
+        {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
         {data.map((platform) => (
-          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+          <MenuItem
+            onClick={() => onSelectedPlatforms(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
         ))}
       </MenuList>
     </Menu>
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -3,7 +3,7 @@ import useData from "./useData";
 export interface Platform {
   id: number;
   name: string;
-  slug: string;
+  slug: number;
 }
 
 const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
