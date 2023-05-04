c4adbde9f2e664f563adc011d1c2bfbacc2db21a
maxymlyskov <raypember02@gmail.com>
2023-05-04 15:39:34 +0300

    Implement search games.

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import theme from "./theme";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
