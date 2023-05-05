0c266bc3de0e6920dd21c83d6b299c5977af269b
maxymlyskov <raypember02@gmail.com>
2023-05-05 16:03:08 +0300

    Implement genres fetching with react-query.

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,19 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import theme from "./theme";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
