57f560351468b5bfa11e0f66f20daf3268a92ca3
maxymlyskov <raypember02@gmail.com>
2023-05-06 23:38:36 +0300

    Refactor: build entities folder.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -8,7 +8,8 @@ import {
   Spinner,
   Text,
 } from "@chakra-ui/react";
-import useGenres, { Genre } from "../hooks/useGenres";
+import useGenres from "../hooks/useGenres";
+import { Genre } from "../entities/Genre";
 import useGameQueryStore from "../store";
 
 const GenreList = () => {
